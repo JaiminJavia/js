d7f5644ca41e43f59292ad7b0d3ce5fb763a6d20
Jaimin Javia <javia.jaimin@gmail.com>
2024-10-06 08:31:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc";
+console.log(typeof score);//both ways are valid to use TYPEOF function
+console.log(typeof(score));//both ways are valid to use TYPEOF function
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); //NAN - NOT A NUMBER
+
+//"33" => 33
+//"33ABC"=> NaN
+//true =>1
+//false => 0
+//1=>true ; 0=>false
+//""=>false
+//"naval"=>true
+
+
+
+
