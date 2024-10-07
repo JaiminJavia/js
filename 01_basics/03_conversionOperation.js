6fbabb49d0aeacbdfe165a93c07d5c416f6597f8
Jaimin Javia <javia.jaimin@gmail.com>
2024-10-07 01:47:19 +0000

    conversion operator and datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33abc";
-console.log(typeof score);//both ways are valid to use TYPEOF function
-console.log(typeof(score));//both ways are valid to use TYPEOF function
+// let score = "33abc";
+// console.log(typeof score);//both ways are valid to use TYPEOF function
+// console.log(typeof(score));//both ways are valid to use TYPEOF function
 
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); //NAN - NOT A NUMBER
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); //NAN - NOT A NUMBER
 
 //"33" => 33
 //"33ABC"=> NaN
@@ -15,5 +15,33 @@ console.log(valueInNumber); //NAN - NOT A NUMBER
 //"naval"=>true
 
 
+//************************OPERATIONS******************************************** */
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2");
+// console.log(+true);//1 true+ = error unexpected token ")"
+// console.log(+"");//=0
+
+
+
+
+//************************comparison******************************************** */
+
+// console.log(null>0);//false
+// console.log(null == 0 );//false
+// console.log(null >=0 );//true --->REASON the reason is that an equality check == and comparison > < >= >= works differently.
+// //comparsion convert null to number treating it as 0 thats why null>=0 is true and null>0 is false
+// console.log(null<=0);//true
+
+
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+// console.log(undefined <= 0);//false
+// console.log(undefined <= 0);//false
+// console.log(undefined >= 0);//false
 
 
---
+++ 01_basics/04_datatypes_summary.js
@@ -0,0 +1,13 @@
+/* on basis of how memory is used in storing data we have categorize dataypes in 2 type primitive and non-primitive*/
+
+//PRIMITIVE - ITS CALL BY VALUE
+//7 TYPES : STRING,NUMBER,BOOLEAN,NULL,UNDEFINED,SYMBOL,BigInt
+
+//NON-PRIMTIVE/REFERENCE TYPE - IN MEMORY WE ALLOT REFERENCE
+//ARRAY,OBJECTS,FUNCTIONS
+
+/*JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. Unlike languages such as Typescript or Java, you don't need to declare the data type of a variable explicitly*/
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefine
+
